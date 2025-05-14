25dc1922d9ccfaf9ba00bde03d1b0218466dc3c4
Brayden Moore <braydenmoore2001@gmail.com>
2025-05-14 12:16:09 -0700

    add last-updated

--- irp.py
+++ irp.py
@@ -587,6 +587,8 @@ async def main_loop():
         with open('info.json', 'r') as f:
             stream_json = json.load(f)
 
+        write_main_page(stream_json)
+
         tasks = [process_stream(name, val) for name, val in stream_json.items()]
         results = await asyncio.gather(*tasks)
 
