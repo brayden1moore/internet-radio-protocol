69197412598721b1479ee9be87494aaafe473428
Brayden Moore <braydenmoore2001@gmail.com>
2025-05-14 10:41:57 -0700

    add shazam guess

--- irp.py
+++ irp.py
@@ -83,7 +83,7 @@ def write_main_page(streams):
             <img width="80px" height="80px" style="margin-right:10px; border: 1px solid black; cursor: pointer;" 
                 src="{v["logo"]}" onclick="toggleAudio('{k}')" />
             <div> 
-                <a target="_blank" href="{v['mainLink']}">{k}</a><br>{to_one_line(v)}<br>{v['shazamGuess']}<br>{v["location"]}<br>{v["status"]}<br>
+                <a target="_blank" href="{v['mainLink']}">{k}</a><br>{to_one_line(v)}<br>{v.get('shazamGuess')}<br>{v["location"]}<br>{v["status"]}<br>
                 <audio id="{k}-audio" style="width:40px;" src="{v["streamLink"]}"></audio>
             </div>
         </div>''' for k, v in streams.items()]),
