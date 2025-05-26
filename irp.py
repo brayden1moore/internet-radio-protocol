669241c2d543050876c472530abc218069b9e276
Brayden Moore <braydenmoore2001@gmail.com>
2025-05-25 23:07:46 -0700

    add LYL

--- irp.py
+++ irp.py
@@ -93,7 +93,7 @@ def write_main_page(streams):
         '</body></html>',
         '<style> @font-face {font-family: "Andale Mono";src: url("assets/andalemono.ttf") format("truetype");}</style>',
         #'<script id="cid0020000408410894191" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 277px;height: 408px;">{"handle":"internetradioprotoco","arch":"js","styles":{"a":"000000","b":100,"c":"FFFFFF","d":"FFFFFF","k":"000000","l":"000000","m":"000000","n":"FFFFFF","p":"10","q":"000000","r":100,"fwtickm":1}}</script>'
-        "<script>function toggleAudio(id){document.querySelectorAll('audio').forEach(a=>{a.pause(); a.src=''; a.parentElement.parentElement.style.backgroundColor='white'});let audio=document.getElementById(`${id}-audio`);let div=document.getElementById(id); audio.src=audio.dataset.src; if (audio.paused) {audio.play();} else {audio.pause();};div.style.backgroundColor='yellow';}</script>",
+        "<script>function toggleAudio(id){document.querySelectorAll('audio').forEach(a=>{a.pause(); a.src=''; a.parentElement.parentElement.style.backgroundColor='white'});let audio=document.getElementById(`${id}-audio`);let div=document.getElementById(id); audio.src=audio.dataset.src; if (audio.paused) {audio.play(); div.style.backgroundColor='yellow';} else {audio.pause();};}</script>",
         "<script>document.querySelectorAll('.last-updated').forEach(el => {const utcStr = el.dataset.utc;if (utcStr) {const date = new Date(utcStr);if (!isNaN(date)) {el.textContent = date.toLocaleString();}}});</script>"]
     )
     with open('index.html', 'w') as f:
