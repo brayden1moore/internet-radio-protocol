7bcbc12e651f53236f79305a42a234c0ddc6b24f
Brayden Moore <braydenmoore2001@gmail.com>
2025-06-12 19:17:13 -0700

    add other email

--- irp.py
+++ irp.py
@@ -67,7 +67,7 @@ def write_main_page(streams):
         '',
         ", ".join(k for k, v in streams.items()) + '.',
         '',
-        'If you have any questions, comments, or radio station addition suggestions, please email <a href="mailto:bmo@scudhouse.com">bmo@scudhouse.com</a>.',
+        'If you have any questions, comments, or radio station addition suggestions, please email <a href="mailto:bmo@scudhouse.com">bmo@scudhouse.com</a> or <a href="mailto:brayden@braydenmoore.com">brayden@braydenmoore.com</a>.',
         '', '',
         '<br>'.join([f'''<div id="{k}" style="align-items: center; display: flex;">
             <img width="110px" height="110px" style="margin-right:10px; border: 1px solid black; cursor: pointer;" 
