1b839e776a3b212ee3adaff4ce0bd75027cdc7b3
Brayden Moore <braydenmoore2001@gmail.com>
2025-07-10 10:58:02 -0700

    header

--- irp.py
+++ irp.py
@@ -77,7 +77,7 @@ def write_main_page(streams):
 
     main_text = ''.join(
         ['<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8"><title>Internet Radio Protocol</title><link rel="icon" href="assets/icon-dark.ico" type="image/x-icon"><link rel="shortcut icon" href="assets/icon-dark.ico" type="image/x-icon"><link rel="apple-touch-icon" href="assets/icon-dark.png"></head><body style="font-family:Andale Mono; padding:10vw; padding-top:10px;"><div style="display:flex; justify-content:center"><img src="assets/scudradiocenter.gif" alt="Loading" height="300px" width="auto"></div>',
-        '<div class="the-header">LISTEN TO<br>COMMUNITY RADIO<br>EVERY DAY</div>', 
+        '<div class="the-header">INTERNET RADIO<br>PROTOCOL</div>', 
         "I love internet radio, so I'm putting it all in one place, like radio-radio. The Internet Radio Protocol is a simple, standardized hub of real-time now playing data and direct streaming links for an ever-expanding list of internet radio stations. Click to tune in. Follow me on instagram, <a target='_blank' href='https://www.instagram.com/scudhouse/'>@scudhouse</a>.",
         '', '',
         '<div class="streams-container">',
