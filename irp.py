fc21e6ac4307e00bcb1658ed6866ea3c5f37e567
Brayden Moore <braydenmoore2001@gmail.com>
2025-07-17 11:19:16 -0700

    Hide some

--- irp.py
+++ irp.py
@@ -102,7 +102,7 @@ def write_main_page(streams):
             <a class="a-link info-link" target="_blank" href="{v['infoLink']}">INFO</a>
             <a class="a-link support-link" target="_blank" href="{v.get('supportLink')}">SUPPORT</a>
         </div>
-        Playing: <span class="now-playing"><span class="one-liner">{v.get('oneLiner')}</span></span><br>
+        <span class="now-playing">Playing: <span class="one-liner">{v.get('oneLiner')}</span></span><br>
         Location: <span class="location">{v["location"]}</span><br>Status: <span class="status">{v["status"]}</span><br>
         <audio id="{v['name']}-audio" style="width:40px;" data-src="{v["streamLink"]}"></audio>
         </div>
