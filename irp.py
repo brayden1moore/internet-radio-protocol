ba1baf9ce64e25701909551cbaf3c5cbfd79b00b
Brayden Moore <braydenmoore2001@gmail.com>
2025-11-17 19:51:37 -0800

    playrandom improvement

--- irp.py
+++ irp.py
@@ -96,7 +96,7 @@ def write_main_page(streams):
         <p id="title">Internet Radio<br>Protocol</p></div>
         ''',
         '<div id="intro-div">'
-        f"The Internet Radio Protocol is a simple, standardized hub of real-time now playing data and direct streaming links for a ever-expanding, curated list of stations. Click a logo to tune in. Support a station if you like it. And follow me on instagram, <a class='insta-link' target='_blank' href='https://www.instagram.com/scud.works/'>@scud.works</a>.</div><br><br>Last updated <span class='last-updated'>{formatted_time}</span>. <span class='live-count'>{len(rerun) + len(online)} online, {len(online)} live, {len(offline)} offline</span>.",
+        f"The Internet Radio Protocol is a standardized hub of real-time now playing data and direct streaming links for a ever-expanding, curated list of stations. Click a logo to tune in. Support a station if you like it. And follow me on instagram, <a class='insta-link' target='_blank' href='https://www.instagram.com/scud.works/'>@scud.works</a>.</div><br><br>Last updated <span class='last-updated'>{formatted_time}</span>. <span class='live-count'>{len(rerun) + len(online)} online, {len(online)} live, {len(offline)} offline</span>.",
         '',
         '''
         <div id="play-random">
