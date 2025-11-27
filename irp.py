667f0d9aa048c8f520a2d46f75b088ca67b213dd
Brayden Moore <braydenmoore2001@gmail.com>
2025-11-27 10:08:47 -0800

    add kusf and doc lines

--- irp.py
+++ irp.py
@@ -748,7 +748,7 @@ class Stream:
             self.now_playing,
             self.now_playing_artist,
             self.now_playing_subtitle,
-            #self.additional_info,
+            self.additional_info,
         ]
         return_string = " - ".join(p for p in parts if p)
         if len(return_string) > 100:
