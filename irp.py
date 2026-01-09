d21941418b6c0351ad2e53e182ca46cc696e9848
Brayden Moore <braydenmoore2001@gmail.com>
2026-01-09 14:14:29 -0800

    process images

--- irp.py
+++ irp.py
@@ -897,7 +897,7 @@ class Stream:
 
         # save images to lib
         for i in ['96','60','25','176']:
-            entire_path = f'{self.name.replace(' ','_')}_{i}.pkl'
+            entire_path = f'logos/{self.name.replace(' ','_')}_{i}.pkl'
             with open(entire_path, 'wb') as f:
                 print(entire_path)
                 pickle.dump(tmp[f'logo_{i}'], f)
