f95e32efb1508a76601fd849301dcfc0eec513b6
Brayden Moore <braydenmoore2001@gmail.com>
2026-06-19 13:26:48 -0700

    rukh link

--- irp.py
+++ irp.py
@@ -934,7 +934,7 @@ class Stream:
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.1 Safari/605.1.15'
             }
-            info = requests.get(self.info_link, headers=headers, timeout=TIMEOUT).text
+            info = requests.get(self.info_link, headers=headers, timeout=TIMEOUT + 10).text
             self.now_playing = info
             self.status = "Live" if self.now_playing else "Offline"
     
