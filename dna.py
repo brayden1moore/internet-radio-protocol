020cc5e11f547b0552e93e72e4ddc832f87f5cb6
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-07 11:03:22 -0700

    styling

--- dna.py
+++ dna.py
@@ -204,7 +204,7 @@ var RADAR_AXIS = {{ radar_axis|tojson }};
           backgroundColor: "rgba(255,255,0,1)",
           borderColor: "#000000",
           borderWidth: 1,
-          pointBackgroundColor: "#FFFF00",
+          pointBackgroundColor: "#000000",
           pointRadius: 4
         }]
       },
