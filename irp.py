664964899c85b111fb714492f9d5482c05e7e320
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-07 14:42:26 -0700

    TK song basis

--- irp.py
+++ irp.py
@@ -1283,7 +1283,7 @@ class Stream:
                 self.now_playing_subtitle = None
                 self.status = 'Live'
                 if info['data']['track_title']:
-                    self.now_playing_subtitle = self.now_playing
+                    self.now_playing_subtitle = None
                     self.now_playing = extract_value(info, ['data','track_title'])
                     self.now_playing_artist = extract_value(info, ['data','track_artist'])
             else:
