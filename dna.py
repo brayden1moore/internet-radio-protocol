d9773ffe7b9557d5ec6c87ec9b56034cc431e15b
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-10 19:58:11 -0700

    styling

--- dna.py
+++ dna.py
@@ -106,7 +106,7 @@ STYLE = """
 
   #spectra-div {
     border-left: none;
-    width: 356px;
+    width: 100%;
   }
 
   #left-div {
@@ -135,7 +135,6 @@ STYLE = """
     #spectra-div {
         margin-left: -1px;
         border-left: 1px solid black;
-        width: 100%;
     }
     #left-div {
         width: 560px;
