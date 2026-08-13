cd0750412f1f9549858a7a4826ec02a8969a411b
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-13 10:02:07 -0700

    email pw

--- check.py
+++ check.py
@@ -112,8 +112,8 @@ def send_email(stations, to_review):
     '''
 
     msg = MIMEMultipart()
-    msg['From'] = 'brayden@one.radio'
-    msg['To'] = 'brayden@one.radio'
+    msg['From'] = 'brayden@braydenmoore.com'
+    msg['To'] = 'brayden@braydenmoore.com'
     msg['Subject'] = f'Review {stations}'
     msg.attach(MIMEText(pprint.pformat(to_review).replace("},","\n\n").replace(": {","\n").replace('{','').replace('}','').replace("'",'').replace('-nowPlaying: ','')))
     try:
