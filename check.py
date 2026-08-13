3565e09c7d970f4af49f3912ed91bcfff5dbd57a
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-13 10:13:41 -0700

    parea

--- check.py
+++ check.py
@@ -53,8 +53,18 @@ def check(v):
         donate_resp = None
 
     try:
+        headers =  {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Dest": "document",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.1 Safari/605.1.15",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Priority": "u=0, i",
+        }
         info_link = v['infoLink'] if 'calendar.google.com' not in v['infoLink'] else f"https://www.googleapis.com/calendar/v3/calendars/{v['infoLink']}/events"
-        info_resp = session.get(info_link, timeout=3).status_code
+        info_resp = session.get(info_link, headers=headers, timeout=3).status_code
     except requests.exceptions.RequestException:
         info_resp = 0
     
