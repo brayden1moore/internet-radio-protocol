1226e558b1e46a14a93f9918800ed9ade5453ac5
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-17 08:47:03 -0700

    GDS

--- irp.py
+++ irp.py
@@ -1598,7 +1598,7 @@ class Stream:
             info = requests.get(self.info_link, timeout=TIMEOUT).json()
             self.now_playing_subtitle = extract_value(info, ['shows','current','name'])
             self.now_playing = extract_value(info, ['tracks','current','metadata','track_title'])
-            self.now_playing = extract_value(info, ['tracks','current','metadata','artist_name'])
+            self.now_playing_artist = extract_value(info, ['tracks','current','metadata','artist_name'])
             self.status = 'Re-Run' if extract_value(info, ['sources','livedj']) == 'off' else 'Live'
 
         ### MARK: STATION LOGIC END
