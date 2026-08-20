98185aaa61b9e5b4cbf23cceb17ec722835705b7
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-19 23:08:55 -0700

    add sticker

--- irp.py
+++ irp.py
@@ -1693,7 +1693,7 @@ class Stream:
 
         # ---- right: white panel with centered ear ----
         ear = Image.open('assets/sticker.png').convert('RGBA')
-        target = int(panel * 0.8)  # fit into 80% of the panel
+        target = int(panel * 1)  # fit into 80% of the panel
         ew, eh = ear.size
         scale = min(target / ew, target / eh)
         ear = ear.resize((int(ew * scale), int(eh * scale)), Image.LANCZOS)
