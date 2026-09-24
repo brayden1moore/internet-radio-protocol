5e84b062118095265d39da553ce28e255635a354
Brayden Moore <braydenmoore2001@gmail.com>
2026-09-23 22:23:59 -0700

    descriptions + dna

--- dna.py
+++ dna.py
@@ -381,7 +381,7 @@ DNA_PANEL = """
       return {
         label: station,
         data: RADAR_DATA[station] || [],
-        backgroundColor: "rgba(255,255,0,1)",
+        backgroundColor: "rgba(0,0,0,1)",
         borderColor: "#000000",
         borderWidth: 1,
         pointBackgroundColor: "#000000",
